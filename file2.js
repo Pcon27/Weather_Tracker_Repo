e094ccec95e01e5d60ab5a7f67264d9e1cbeb143
pconsag <pconsagra@g.ucla.edu>
2021-07-05 20:11:46 -0700

    add api fetch, and add data to elements needed for weather forcast

--- file2.js
+++ file2.js
@@ -1,62 +1,31 @@
 var apiKey = "2fe2a660ccee4f29bd7d0d10e5775d14"
-
-var temp = ("#temp")
-
-
-
-// var city = $("#exampleDataList").val;
-
-// var city = "Los Angeles"
+var temp = (".temp")
+var tempInput = ("#tempInput")
 
 
 $("#button").on("click", function(event) {
-
     event.preventDefault();
 
     var city = $("#cityInput").val()
+    var URL = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=imperial&appid=' + apiKey
     console.log(city);
 
-   
+    fetch(URL)
 
-    fetch('https://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=imperial&appid=' + apiKey)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-        console.log('Open Weather \n----------');
-        //We need to examine the API response to know how to access the data we want to extract.
+        .then(function (response) {
+      return response.json()
+      .then(function(data){
         console.log(data);
-        for (var i = 0; i < data.length; i++) {
-            console.log(data[i].name);
-            
-          }
-        })
-        temp.text({temp} = data.main);
-})
+        var {}
+    })}
 
 
 
-
-
-//     fetch('https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey)
-//   .then(function (response) {
-//     return response.json();
-//   })
-//   .then(function (data) {
-//     console.log('Open Weather \n----------');
-//     //We need to examine the API response to know how to access the data we want to extract.
-//     console.log(data);
-//     for (var i = 0; i < data.length; i++) {
-//         console.log(data[i].name);
-//       }
-//     })});
-
-
-// fetch('https://api.github.com/orgs/twitter/repos')
-//   .then(function (response) {
-//     return response.json();
-//   })
-//   .then(function (data) {
-//     console.log('Twitter Repositories: Names only \n----------');
-//     console.log(data);
-//     //The response we receive is an array, meaning that we can loop through the data, accessing whatever specific data we want, as shown in the following code:
+   
+    // .then(function (data) {
+    //     console.log('Open Weather \n----------');
+    //     //We need to examine the API response to know how to access the data we want to extract.
+    //     console.log(data);
+    //     for (var i = 0; i < data.length; i++) {
+    //         console.log(data[i].name);
+    //         }
