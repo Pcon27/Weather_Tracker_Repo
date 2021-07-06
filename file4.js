25a4946fd65f4075c2c22cf8e55760240b9e0365
pconsag <pconsagra@g.ucla.edu>
2021-07-05 20:44:59 -0700

    link name put into form on HTML to city used in weather data

--- file4.js
+++ file4.js
@@ -1,3 +1,20 @@
 var apiKey = "2fe2a660ccee4f29bd7d0d10e5775d14"
 var cityName = $("#city")
 var tempInput = $("#tempInput")
+
+$("#button").on("click", function(event) {
+    event.preventDefault();
+
+    var city = $("#cityInput").val()
+    var URL1 = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=imperial&appid=' + apiKey;
+    console.log(city);
+
+
+
+
+
+
+
+
+
+})
