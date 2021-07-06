49985709f41b110c40b7d8d37be2314b60aa2856
pconsag <pconsagra@g.ucla.edu>
2021-07-06 14:49:59 -0700

    add date to each forcast

--- file4.js
+++ file4.js
@@ -1,11 +1,18 @@
 var apiKey = "2fe2a660ccee4f29bd7d0d10e5775d14"
 var cityName = $("#city")
-var tempInput = $("#tempInput")
+var currentWeather = $(".currentWeather")
 var forcast1 = $("#forcast1")
 var forcast2 = $("#forcast2")
 var forcast3 = $("#forcast3")
 var forcast4 = $("#forcast4")
 var forcast5 = $("#forcast5")
+var weatherIcon = $("#weatherIcon") 
+var todaysDate = moment();
+var day1Date = moment().add(1, 'days')
+var day2Date = moment().add(2, 'days')
+var day3Date = moment().add(3, 'days')
+var day4Date = moment().add(4, 'days')
+var day5Date = moment().add(5, 'days')
 
 
 // start function on the click of the seach function of HTML form
@@ -31,11 +38,17 @@ $("#button").on("click", function(event) {
 
         var { name } = data
         var { temp } = data.main
-        // var { icon } = data
+        var { humidity } = data.main
+        var { speed } = data.wind
+        var { icon } = data.weather[0]
     
-       cityName.text(name);
-        tempInput.text("Current Temp: " + temp);
-        console.log(temp);
+        currentWeather.append("<h2>" + name + "</h2");
+        $("#todaysDate").text(todaysDate.format("MMM Do, YYYY"));
+        currentWeather.append("<p>" + "Current Temp: " + temp + "</p");
+        currentWeather.append("<p>" + "Humidity : " + humidity +  "%" + "</p>")
+        currentWeather.append("<p>" + "Wind Speed : " + speed + "</p>")
+        weatherIcon.attr("src", "https://openweathermap.org/img/wn/" + icon + "@2px.png")
+
        
         var { lat } = data.coord
         var { lon } =  data.coord
@@ -58,37 +71,41 @@ $("#button").on("click", function(event) {
             for (var i = 1; i < 6; i++) {
                 var { icon } = data2.daily[i].weather[0]
                 var { day } = data2.daily[i].temp
-                var { humidity } =data2.daily[i]
+                var { humidity } = data2.daily[i]
+                var { uvi } = data2.current
                 console.log(icon)
                 console.log(day)
 
                 if (i===1){
+                    currentWeather.append("<p>" + "UV Index : " + uvi + "</p>")
+                    forcast1.append("<p>" + day1Date.format("MMM Do, YYYY") + "</p>")
                     forcast1.append("<p>" + "temp : " + day +  "</p>")
-                    forcast1.append("<p>" + "humidity : " + humidity +  "</p>")
-
+                    forcast1.append("<p>" + "humidity : " + humidity +  "%" + "</p>")
                 }
 
                 if (i===2){
+                    forcast2.append("<p>" + day2Date.format("MMM Do, YYYY") + "</p>")
                     forcast2.append("<p>" + "temp : " + day +  "</p>")
-                    forcast2.append("<p>" + "humidity : " + humidity +  "</p>")
-
+                    forcast2.append("<p>" + "humidity : " + humidity +  "%" + "</p>")
                 }
 
                 if (i===3){
+                    forcast3.append("<p>" + day3Date.format("MMM Do, YYYY") + "</p>")
                     forcast3.append("<p>" + "temp : " + day +  "</p>")
-                    forcast3.append("<p>" + "humidity : " + humidity +  "</p>")
+                    forcast3.append("<p>" + "humidity : " + humidity +  "%" + "</p>")
                 }
 
                 if (i===4){
+                    forcast4.append("<p>" + day4Date.format("MMM Do, YYYY") + "</p>")
                     forcast4.append("<p>" + "temp : " + day +  "</p>")
-                    forcast4.append("<p>" + "humidity : " + humidity +  "</p>")
+                    forcast4.append("<p>" + "humidity : " + humidity +  "%" + "</p>")
                 }
 
                 if (i===5){
+                    forcast5.append("<p>" + day5Date.format("MMM Do, YYYY") + "</p>")
                     forcast5.append("<p>" + "temp : " + day +  "</p>")
-                    forcast5.append("<p>" + "humidity : " + humidity +  "</p>")
+                    forcast5.append("<p>" + "humidity : " + humidity +  "%" + "</p>")
                 }
-
 
             }
         })
